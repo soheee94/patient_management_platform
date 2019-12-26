0fa6d2b66ec52778293325a986a5854099928ef2
soheee94 <hsh8616@naver.com>
2019-12-26 18:16:38 +0900

    create Tutorial Component

--- src/components/PatientManagement/Nav.js
+++ src/components/PatientManagement/Nav.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { MdHelpOutline } from "react-icons/md";
+import Tutorial from "./Tutorial";
 
 function Nav() {
   const today = new Date();
@@ -10,26 +11,37 @@ function Nav() {
     weekday: "long"
   });
 
-  const [isOpen, setOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isTutorialOpen, setTutorialOpen] = useState(false);
 
   return (
-    <div className="nav">
-      <p className="nav__date">{dateString}</p>
-      <div className="nav__right">
-        <div className={isOpen ? "nav__right__dropdown active" : "nav__right__dropdown"}>
-          <div className="nav__right__dropdown__text" onClick={() => setOpen(!isOpen)}>
-            바른손정형외과
+    <>
+      <div className="nav">
+        <p className="nav__date">{dateString}</p>
+        <div className="nav__right">
+          <div className={isDropdownOpen ? "nav__right__dropdown active" : "nav__right__dropdown"}>
+            <div
+              className="nav__right__dropdown__text"
+              onClick={() => setDropdownOpen(!isDropdownOpen)}
+            >
+              바른손성형외과
+            </div>
+            <div className="nav__right__dropdown__items">
+              <div className="nav__right__dropdown__item">병원정보</div>
+              <div className="nav__right__dropdown__item">로그아웃</div>
+            </div>
           </div>
-          <div className="nav__right__dropdown__items">
-            <div className="nav__right__dropdown__item">병원정보</div>
-            <div className="nav__right__dropdown__item">로그아웃</div>
+          <div
+            className="nav__right__tutorial-btn"
+            onClick={() => setTutorialOpen(!isTutorialOpen)}
+          >
+            <MdHelpOutline />
           </div>
-        </div>
-        <div className="nav__right__tutorial-btn">
-          <MdHelpOutline />
         </div>
       </div>
-    </div>
+
+      {isTutorialOpen && <Tutorial />}
+    </>
   );
 }
 
---
+++ src/components/PatientManagement/Tutorial.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tutorial() {
+  return <div>나는 튜토리얼</div>;
+}
+
+export default Tutorial;
