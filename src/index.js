ee7b878d5c0caa8af4bc399f9e14c000b47e96f2
soheee94 <hsh8616@naver.com>
2019-12-26 15:51:38 +0900

    add scss files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+import "./styles/main.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
