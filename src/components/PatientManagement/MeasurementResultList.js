a723bfae4054ed0c3baa5bc50158f9b6a7ea3331
한소희 <hsh8616@naver.com>
2020-01-10 18:57:33 +0900

    add MeasurementResultList Background

--- src/components/PatientManagement/MeasurementResultList.js
+++ src/components/PatientManagement/MeasurementResultList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import logo_big from "../../assets/logo_big.png";
 import ListTitle from "./ListTitle";
 import Button from "../Button";
+
+const ListBackground = styled.div`
+  height: calc(100% - 50px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 60%;
+  }
+`;
 
 function MeasurementResultList() {
   return (
@@ -9,9 +20,9 @@ function MeasurementResultList() {
       <ListTitle style={{ justifyContent: "flex-end" }}>
         <Button color="pink">측정 결과</Button>
       </ListTitle>
-      <div className="list-content-background">
+      <ListBackground>
         <img src={logo_big} alt="배경 로고" />
-      </div>
+      </ListBackground>
     </>
   );
 }
